33a92572817e9e36bf517e334ead034b996b2a87
unknown <mxuanvan159@gmail.com>
2022-03-24 16:42:55 +0700

    Completed design UI.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img class="rounded-circle" style="width: 150px; height: 150px" src="https://lh3.googleusercontent.com/xoSQwx1jcAKlwPbKVHU--1vuEEZ26fih4m4x7I-31TdkNT9-ZjhlR9AoulTJlpHAf9VBti58iwUYuqsrCi4r4be5Put5DeJzmjYl9nsIdbBpd2kS8LlTdX8tvDnb7AW3eSdQMUJsKP665F1-w24F30lSXnz_7hhxaA7xa1_mIolOr3Md2jXZVZizV2IW6dFtxefx0e6yQuzsRBZflu3khv1rJWXHnrPeTBg7KuILc5i64tZ1_jjSZQVg0Rz0bf_8q04QRoONh18eEBeZk4ZJXGnfhKaMjVh-RuWmCUIyb-fHt3AEQBFWUfLSCPvVHIf4jn-QccRJpBInYjfJ4pdOhkiH7ixVLsEM3v78-WNvTYkKuPY140zVRDbmy9Gc-KDc4H85p6_Kw6twne5nmsMpcroWjixKNXm1qt9aBwQiSXgeN5DA4gsqIzSbi354wk0H5DAmyek44Hr42OCxPxkWqfqcg6kVaS51pDUq3sB3dfAWd8mgCRSvouG9ehRea5cZuDlgns-3wnS8-Gsa6EHMNwRWhf9orED4HJjxtFpuDEdz2USaAeZxiGGuwyp2HFwpmDKmc8tBxWXBrc2EY9oZ9x0RIU0T22tkPee8EPMLzGVwzW7PIvYOPiWicp3HNYXtgn3PvBtj00GT0mTIO3pRhbOlxDDpwNIL1sL5TSK0340foUSy_IVfb4TrkDDglfXz5hu9zsnlwqqCNE6kQSmNdEuuOJmsoITZa8Epy_xbvqEIXasgLnATxg=w878-h873-no?authuser=0">
+        </div>
+        <div class="col-9 pt-5 ps-5">
+            <div><h1>xuanzannn</h1></div>
+            <div class="d-flex pt-3">
+                <div class="pe-4"><strong>1234</strong> posts</div>
+                <div class="pe-4"><strong>234k</strong> followers</div>
+                <div class="pe-4"><strong>1</strong> following</div>
+            </div>
+            <div class="fw-bold pt-4">Xuan Van</div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img class="w-100" src="https://lh3.googleusercontent.com/irab00MmXCLL70yqpTKTs2uc_mRCK9PKbjwWf40wA_b2nKJXKfU8K22b9EjFTpd0sNopzFUmVd4KetHnGapsYL-FmFXPjbkCFIVQoR8FV1GWQZ45iqmFHLLbvRJ-s3ohEXLlCXDQJDceF3XHQkAdj3go2zz_fPfZhRJDop3Me9e265jCRTDd8ujwcrn4rwFIwCsZ5JVT8WVLDYK7ExTfLXzeVxhhzLc5HH0fUHIW_Ec6lZnr2jSpYu3V775mTVuw9LresC-DYwdPqMBtF8YfYMYJk1E4JgY0AUyTn2eqEhkPPHFURkHphB-b2eGuLwvmfBGg7bda6NcAKSisUuv-IiGrqkLK6y1P-RFCFWuWxsB3KK3ieKNzpbtHtIUWyPz2vTbUq9xzwkF6ZDmb7ZS2sxmz0w7SV825tJS6V_VD8iO0Q31enYDb8FC0MLbRwIHN0CKXgpAtWHLDhbzWxNVrA51x5AiZDq4e_QLG4ffM8WKerrg89WS5dLHm6SRELpvvXuV3NVGAlbXm5tCeS6f0JT-8JMZRe1RGPB_TtwoNEACwiUinaYYdxwjT4nqlW13gqChPCwkft3oLx9teXDs2t7GxfJqAMSGaYtLtqpNkeFTbUX8Z6Zf978goHdpQKMJOsRwHkMzuMpcALMNweeSKjrltL581d_tvd_fNWVvzaVt0Cb2qO0B5ef1DiW0hmAbXI9z6poAISnbtD-KkZ8AMlMUIL4e4O5QWy3cwTtbUEaH97FaRaad97A=w709-h873-no?authuser=0">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://lh3.googleusercontent.com/irab00MmXCLL70yqpTKTs2uc_mRCK9PKbjwWf40wA_b2nKJXKfU8K22b9EjFTpd0sNopzFUmVd4KetHnGapsYL-FmFXPjbkCFIVQoR8FV1GWQZ45iqmFHLLbvRJ-s3ohEXLlCXDQJDceF3XHQkAdj3go2zz_fPfZhRJDop3Me9e265jCRTDd8ujwcrn4rwFIwCsZ5JVT8WVLDYK7ExTfLXzeVxhhzLc5HH0fUHIW_Ec6lZnr2jSpYu3V775mTVuw9LresC-DYwdPqMBtF8YfYMYJk1E4JgY0AUyTn2eqEhkPPHFURkHphB-b2eGuLwvmfBGg7bda6NcAKSisUuv-IiGrqkLK6y1P-RFCFWuWxsB3KK3ieKNzpbtHtIUWyPz2vTbUq9xzwkF6ZDmb7ZS2sxmz0w7SV825tJS6V_VD8iO0Q31enYDb8FC0MLbRwIHN0CKXgpAtWHLDhbzWxNVrA51x5AiZDq4e_QLG4ffM8WKerrg89WS5dLHm6SRELpvvXuV3NVGAlbXm5tCeS6f0JT-8JMZRe1RGPB_TtwoNEACwiUinaYYdxwjT4nqlW13gqChPCwkft3oLx9teXDs2t7GxfJqAMSGaYtLtqpNkeFTbUX8Z6Zf978goHdpQKMJOsRwHkMzuMpcALMNweeSKjrltL581d_tvd_fNWVvzaVt0Cb2qO0B5ef1DiW0hmAbXI9z6poAISnbtD-KkZ8AMlMUIL4e4O5QWy3cwTtbUEaH97FaRaad97A=w709-h873-no?authuser=0">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://lh3.googleusercontent.com/irab00MmXCLL70yqpTKTs2uc_mRCK9PKbjwWf40wA_b2nKJXKfU8K22b9EjFTpd0sNopzFUmVd4KetHnGapsYL-FmFXPjbkCFIVQoR8FV1GWQZ45iqmFHLLbvRJ-s3ohEXLlCXDQJDceF3XHQkAdj3go2zz_fPfZhRJDop3Me9e265jCRTDd8ujwcrn4rwFIwCsZ5JVT8WVLDYK7ExTfLXzeVxhhzLc5HH0fUHIW_Ec6lZnr2jSpYu3V775mTVuw9LresC-DYwdPqMBtF8YfYMYJk1E4JgY0AUyTn2eqEhkPPHFURkHphB-b2eGuLwvmfBGg7bda6NcAKSisUuv-IiGrqkLK6y1P-RFCFWuWxsB3KK3ieKNzpbtHtIUWyPz2vTbUq9xzwkF6ZDmb7ZS2sxmz0w7SV825tJS6V_VD8iO0Q31enYDb8FC0MLbRwIHN0CKXgpAtWHLDhbzWxNVrA51x5AiZDq4e_QLG4ffM8WKerrg89WS5dLHm6SRELpvvXuV3NVGAlbXm5tCeS6f0JT-8JMZRe1RGPB_TtwoNEACwiUinaYYdxwjT4nqlW13gqChPCwkft3oLx9teXDs2t7GxfJqAMSGaYtLtqpNkeFTbUX8Z6Zf978goHdpQKMJOsRwHkMzuMpcALMNweeSKjrltL581d_tvd_fNWVvzaVt0Cb2qO0B5ef1DiW0hmAbXI9z6poAISnbtD-KkZ8AMlMUIL4e4O5QWy3cwTtbUEaH97FaRaad97A=w709-h873-no?authuser=0">
+        </div>
+    </div>
+</div>
+@endsection
